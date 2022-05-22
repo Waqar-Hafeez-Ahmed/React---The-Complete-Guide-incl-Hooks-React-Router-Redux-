44829460911a494fa5d767d7ae09c7ea1dd58686
Waqar Ahmed <waqarhafeez12@gmail.com>
2022-05-22 15:47:23 +0500

    This is the first commit of Visual Studio

---
+++ src/Expenses.js
@@ -0,0 +1,31 @@
+import Card from './components/Card';
+import ExpenseItem from './components/ExpenseItem';
+import './Expenses.css'
+
+
+function Expenses(props) {
+  return <Card className='expenses'>
+    <ExpenseItem
+      title={props.item[0].title}
+      date={props.item[0].date}
+      amount={props.item[0].amount}
+    />
+    <ExpenseItem
+      title={props.item[1].title}
+      date={props.item[1].date}
+      amount={props.item[1].amount}
+    />
+    <ExpenseItem
+      title={props.item[2].title}
+      date={props.item[2].date}
+      amount={props.item[2].amount}
+    />
+    <ExpenseItem
+      title={props.item[3].title}
+      date={props.item[3].date}
+      amount={props.item[3].amount}
+    />
+  </Card>;
+}
+
+export default Expenses;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from "./Date";
+import Card from "./Card";
+import "./ExpenseItem.css"
+
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log('Clicked!!');
+  }
+  
+  return (
+    <Card className="expense-item">
+     <ExpenseDate date = {props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}> Change Title! </button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
